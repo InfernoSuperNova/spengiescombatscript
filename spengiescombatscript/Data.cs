aebe50b1743d953e5e350024d576fbc3d47304b7
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2024-04-08 22:21:14 +1200

    Bad

--- spengiescombatscript/Data.cs
+++ spengiescombatscript/Data.cs
@@ -7,8 +7,10 @@ using VRageMath;
 
 namespace IngameScript
 {
+    // this whole class shouldn't exist, but it's a hack to get around my shitty code
     public static class Data
     {
         public static Vector3D prevTargetVelocity = Vector3D.Zero;
+        public static Vector3D aimPos = Vector3D.Zero;
     }
 }
